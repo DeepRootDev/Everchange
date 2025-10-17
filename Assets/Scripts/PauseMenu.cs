f577f73e864150bbfef8bf7e544895f165994674
Christer Kaitila <breakdance@mcfunkypants.com>
2025-10-17 11:46:29 -0700

    coded pause menu buttons (resume, main menu, restart, exit)

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
+using UnityEditor; 
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     [Header("Pause Menu activated on [ESC] or [P]")]
-    public GameObject pauseGUI; 
+    public GameObject pauseGUI;
+
+
+    [Header("Be sure to include this scene in the project build settings")]
+    public string nameOfMainMenuScene = "MainMenu";
+
     private bool isPaused = false;
 
     void Update()
@@ -37,8 +44,30 @@ public class PauseMenu : MonoBehaviour
         pauseGUI.SetActive(false);
         isPaused = false;
     }
+
+    public void clickMainMenuButton()
+    {
+        Debug.Log("Main Menu Button was clicked!");
+        SceneManager.LoadScene(nameOfMainMenuScene);
+    }
+    public void clickRestartButton()
+    {
+        Debug.Log("Restart Button was clicked!");
+        // FIXME: not sure how to reset everything using waypoint drive etc
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void clickExitButton()
+    {
+        Debug.Log("Exit Button was clicked!");
+        #if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
 }
-
-
-
 
