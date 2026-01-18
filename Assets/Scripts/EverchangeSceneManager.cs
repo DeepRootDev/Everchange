f35b037b9d32b6edfd5771485103a2db7d985b29
IanCher <IanCher@users.noreply.github.com>
2026-01-18 19:01:16 +0100

    Add menu button beep when clicking button in menu

--- Assets/Scripts/EverchangeSceneManager.cs
+++ Assets/Scripts/EverchangeSceneManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,16 +7,23 @@ public class EverchangeSceneManager : MonoBehaviour
     [TextArea] [SerializeField] private string note = "Contains logic to switch between scenes. Enter the names of the scenes down below. Names must with the scenes in the scenes/ folder!";
     
     [SerializeField] private string loadingSceneName = "Loading Scene";
+    [SerializeField] private float timeBeforeLoadingScene = 0.1f; // small time to wait to allow for hearing button sounds 
     
     public void LoadLoadingScene()
     {
-        SceneManager.LoadScene(loadingSceneName);
+        StartCoroutine(LoadSceneAfterSecond(loadingSceneName));
     }
 
     // debug version only
     public void LoadTestPlayground()
     {
         SceneManager.LoadScene("Test Playground");
+    }
+
+    private IEnumerator LoadSceneAfterSecond(string sceneName)
+    {
+        yield return new WaitForSeconds(timeBeforeLoadingScene);
+        SceneManager.LoadScene(loadingSceneName);
     }
 
 }
