477851481bdfc4fbfd6faf8096d22e25196202a0
Clayton Anderson <ckanders@icloud.com>
2026-01-21 17:35:50 -0500

    Add song player

---
+++ Assets/Scripts/SongPlayer.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+
+public class SongPlayer : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    [Header("Plays once on game start")]
+    public AudioClip FirstSong;
+    [Header("Plays on loop after FirstSong")]
+    public AudioClip SecondSong;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        if (audioSource == null)
+            audioSource = gameObject.AddComponent<AudioSource>();
+
+        audioSource.loop = false;
+    }
+
+    void Start()
+    {
+        StartCoroutine(PlayMusicSequence());
+    }
+
+    IEnumerator PlayMusicSequence()
+    {
+        audioSource.clip = FirstSong;
+        audioSource.loop = false;
+        audioSource.Play();
+
+        yield return new WaitForSeconds(FirstSong.length);
+
+        audioSource.clip = SecondSong;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+}
