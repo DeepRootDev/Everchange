ea3089d9f7ea51bda261723305959ffc3339e614
bariskoklu <30669414+bariskoklu@users.noreply.github.com>
2026-01-25 13:58:07 +0300

    pickup item can be randomized on pickup when you select the isRandom variable

--- Assets/PickUpItemScript.cs
+++ Assets/PickUpItemScript.cs
@@ -5,24 +5,28 @@ public class PickUpItemScript : MonoBehaviour
 {
 
     [SerializeField]
-    private PickUpItemColors randomColor;
-    void Start()
-    {
-        randomColor = (PickUpItemColors)UnityEngine.Random.Range(0, Enum.GetNames(typeof(PickUpItemColors)).Length - 1);
-    }
+    private PickUpItemColors color;
+    [SerializeField]
+    [Tooltip("If this is set to true, when the object gets picked up the color will be random")]
+    private bool isRandom = true;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Player picked up " + randomColor.ToString() + " Item");
+            if (isRandom) 
+            {
+                color = (PickUpItemColors)UnityEngine.Random.Range(0, Enum.GetNames(typeof(PickUpItemColors)).Length - 1);
+            }
+
+            Debug.Log("Player picked up " + color.ToString() + " Item");
 
             //We might want a parent class for PlayerPowerUpManager and whatever class the AI will have for the manager so we can have one unified variable and method for both
             //If not AI will need different code here.
             other.TryGetComponent<PlayerPowerUpManager>(out PlayerPowerUpManager playerPowerUpManager);
             if (playerPowerUpManager != null)
             {
-                playerPowerUpManager.AddPowerUp(randomColor);
+                playerPowerUpManager.AddPowerUp(color);
             }
         }
     }
