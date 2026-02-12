cbb77bc7b1ec38711ebd2222b20b0370c2e8cca9
Fahad Muntaz <fmuntaz@gmail.com>
2026-02-12 11:19:47 -0500

    [WIP} Add animated pulse object to checkpoints
    Move sphere on line between checkpoints

--- Assets/Scripts/TrackCheckpoint.cs
+++ Assets/Scripts/TrackCheckpoint.cs
@@ -8,6 +8,13 @@ public class TrackCheckpoint : MonoBehaviour
     public float lineLength = 0;
     public Boolean invertVisualLine;
     public float width = 3;
+    private GameObject animatedPulseObject;
+    float timer;
+    private float duration = 3f;
+    Vector3 pulseObjectStartingPosition;
+    Vector3 pulseObjectEndPosition;
+    float pulseObjectEndPositionXOffset = 2.0f;
+    Vector3 lineEndPosition;
 
     void Start()
     {
@@ -21,7 +28,45 @@ public class TrackCheckpoint : MonoBehaviour
         {
             lineLength *= -1;
         }
+       
         visualLineCheckpoint.SetPosition(0,startingPosition);
-        visualLineCheckpoint.SetPosition(1,new Vector3(lineLength,startingPositionY,0f));
+        lineEndPosition = new Vector3(lineLength,startingPositionY,0f);
+        visualLineCheckpoint.SetPosition(1,lineEndPosition);
+        
+        Transform child = transform.Find("animatedPulseObject");
+        if (child != null)
+        {
+            animatedPulseObject = child.gameObject;
+            timer = 0f;
+            pulseObjectStartingPosition = animatedPulseObject.transform.localPosition;
+            pulseObjectEndPosition = pulseObjectStartingPosition + new Vector3((lineEndPosition.x - pulseObjectEndPositionXOffset), 0, 0);
+        }
+
+    }
+
+    void Update()
+    {
+        if (animatedPulseObject != null)
+        {
+            if (timer < duration)
+            {
+                timer += Time.deltaTime;
+                float t = Mathf.Clamp01(timer / duration); 
+                animatedPulseObject.transform.localPosition = Vector3.Lerp(pulseObjectStartingPosition, pulseObjectEndPosition, t);
+            }
+            else
+            {
+                ResetAnimatedPulseObject();
+            }
+
+        }
+    }
+
+    void ResetAnimatedPulseObject()
+    {
+        animatedPulseObject.SetActive(false);
+        animatedPulseObject.transform.position = pulseObjectStartingPosition;
+        animatedPulseObject.SetActive(true);
+        timer = 0f;
     }
 }
