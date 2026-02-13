7d781b701fad2bee73f0df124484a4a99803ff05
Fahad Muntaz <fmuntaz@gmail.com>
2026-02-12 20:48:26 -0500

    [DONE] Update track checkpoint to draw line automatically to next checkpoint

--- Assets/Scripts/TrackCheckpoint.cs
+++ Assets/Scripts/TrackCheckpoint.cs
@@ -1,65 +1,75 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TrackCheckpoint : MonoBehaviour
 {
     LineRenderer visualLineCheckpoint;
-    float startingPositionY = -60f;
     public float lineLength = 0;
-    public Boolean invertVisualLine;
     public float width = 3;
+    public Transform TheNextCheckpoint;
     private GameObject animatedPulseObject;
     float timer;
-    private float duration = 1.2f;
+    private float duration = 20f;
     Vector3 pulseObjectStartingPosition;
-    Vector3 pulseObjectEndPosition;
-    float pulseObjectEndPositionXOffset = 4.0f;
     Vector3 lineEndPosition;
 
     void Start()
     {
-        visualLineCheckpoint = GetComponent<LineRenderer>();
-        visualLineCheckpoint.positionCount = 2;
-        visualLineCheckpoint.startWidth = width;
-        visualLineCheckpoint.endWidth = width;
+
+        if (TheNextCheckpoint)
+        {
+            visualLineCheckpoint = GetComponent<LineRenderer>();
+            visualLineCheckpoint.positionCount = 2;
+            visualLineCheckpoint.startWidth = width;
+            visualLineCheckpoint.endWidth = width;
       
-        Vector3 startingPosition = new Vector3(0f, startingPositionY, 0f);
-        if (invertVisualLine)
-        {
-            lineLength *= -1;
+            Transform anchorObj = transform.Find("anchorLinePosition");
+            visualLineCheckpoint.SetPosition(0,anchorObj.position);
+            
+            Transform checkPointAnchor = TheNextCheckpoint.transform.Find("anchorLinePosition");
+            lineEndPosition = checkPointAnchor.position;
+            
+            visualLineCheckpoint.SetPosition(1,lineEndPosition);
+            
+            //TODO:FIXME
+            // Transform arrow = transform.Find("animatedPulseObject");
+            // if (arrow != null)
+            // {
+            //     animatedPulseObject = arrow.gameObject;
+            //     timer = 0f;
+            //     pulseObjectStartingPosition = animatedPulseObject.transform.localPosition;
+            //     pulseObjectEndPosition = pulseObjectStartingPosition + lineEndPosition;
+            // }
         }
-       
-        visualLineCheckpoint.SetPosition(0,startingPosition);
-        lineEndPosition = new Vector3(lineLength,startingPositionY,0f);
-        visualLineCheckpoint.SetPosition(1,lineEndPosition);
-        
-        Transform child = transform.Find("animatedPulseObject");
-        if (child != null)
+        else
         {
-            animatedPulseObject = child.gameObject;
-            timer = 0f;
-            pulseObjectStartingPosition = animatedPulseObject.transform.localPosition;
-            pulseObjectEndPosition = pulseObjectStartingPosition + new Vector3((lineEndPosition.x - pulseObjectEndPositionXOffset), 0, 0);
+            Transform arrow = transform.Find("animatedPulseObject");
+            if (arrow != null)
+            {
+                arrow.gameObject.SetActive(false);
+            }
         }
 
     }
 
     void Update()
     {
-        if (animatedPulseObject != null)
-        {
-            if (timer < duration)
-            {
-                timer += Time.deltaTime;
-                float t = Mathf.Clamp01(timer / duration); 
-                animatedPulseObject.transform.localPosition = Vector3.Lerp(pulseObjectStartingPosition, pulseObjectEndPosition, t);
-            }
-            else
-            {
-                ResetAnimatedPulseObject();
-            }
+        //TODO: FIXME
+        // if (TheNextCheckpoint != null)
+        // {
+        //     if (timer < duration)
+        //     {
+        //         timer += Time.deltaTime;
+        //         float t = Mathf.Clamp01(timer / duration); 
+        //         animatedPulseObject.transform.localPosition = Vector3.Lerp(pulseObjectStartingPosition, pulseObjectEndPosition, t);
+        //     }
+        //     else
+        //     {
+        //         ResetAnimatedPulseObject();
+        //     }
 
-        }
+        // }
     }
 
     void ResetAnimatedPulseObject()
