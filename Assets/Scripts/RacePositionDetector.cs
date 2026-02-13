beea530e718c1cb1f7a93d7e399952d3b506ff61
Christer Kaitila <breakdance@mcfunkypants.com>
2026-02-13 14:57:32 -0800

    up to 10 ai opponents

--- Assets/Scripts/RacePositionDetector.cs
+++ Assets/Scripts/RacePositionDetector.cs
@@ -20,12 +20,18 @@ public class RacePositionDetector : MonoBehaviour
     [Header("The position of the finish line:")]
     public Transform theFinishLine;
 
-    [Header("The Racers!")]
+    [Header("The Racers! (optional)")]
     public PlayerMovement thePlayer;
     public WaypointDrive opponent1;
     public WaypointDrive opponent2;
     public WaypointDrive opponent3;
     public WaypointDrive opponent4;
+    public WaypointDrive opponent5;
+    public WaypointDrive opponent6;
+    public WaypointDrive opponent7;
+    public WaypointDrive opponent8;
+    public WaypointDrive opponent9;
+    public WaypointDrive opponent10;
 
     private float waypointMaxDistToTrigger = 30; // FIXME: use a trigger collider instead?
     private int currentPosition = -1;
@@ -96,7 +102,81 @@ public class RacePositionDetector : MonoBehaviour
                 if (rubberband4) opponent4.pleaseSpeedUp();
             }
         }
-        
+
+        if (opponent5) {
+            bool rubberband5 = Vector3.Distance(opponent5.transform.position,thePlayer.transform.position) > tooFar;
+            if (thePlayer.hasPassedWaypointNumber < opponent5.hasPassedWaypointNumber
+            || (thePlayer.hasPassedWaypointNumber == opponent5.hasPassedWaypointNumber && thePlayer.distanceToNextWaypoint > opponent5.distanceToNextWaypoint)
+            ) {
+                if (rubberband5) opponent5.pleaseSlowDown();
+                rank++;
+            } else {
+                if (rubberband5) opponent5.pleaseSpeedUp();
+            }
+        }
+
+        if (opponent6) {
+            bool rubberband6 = Vector3.Distance(opponent6.transform.position,thePlayer.transform.position) > tooFar;
+            if (thePlayer.hasPassedWaypointNumber < opponent6.hasPassedWaypointNumber
+            || (thePlayer.hasPassedWaypointNumber == opponent6.hasPassedWaypointNumber && thePlayer.distanceToNextWaypoint > opponent6.distanceToNextWaypoint)
+            ) {
+                if (rubberband6) opponent6.pleaseSlowDown();
+                rank++;
+            } else {
+                if (rubberband6) opponent6.pleaseSpeedUp();
+            }
+        }
+
+        if (opponent7) {
+            bool rubberband7 = Vector3.Distance(opponent7.transform.position,thePlayer.transform.position) > tooFar;
+            if (thePlayer.hasPassedWaypointNumber < opponent7.hasPassedWaypointNumber
+            || (thePlayer.hasPassedWaypointNumber == opponent7.hasPassedWaypointNumber && thePlayer.distanceToNextWaypoint > opponent7.distanceToNextWaypoint)
+            ) {
+                if (rubberband7) opponent7.pleaseSlowDown();
+                rank++;
+            } else {
+                if (rubberband7) opponent7.pleaseSpeedUp();
+            }
+        }
+
+        if (opponent8) {
+            bool rubberband8 = Vector3.Distance(opponent8.transform.position,thePlayer.transform.position) > tooFar;
+            if (thePlayer.hasPassedWaypointNumber < opponent8.hasPassedWaypointNumber
+            || (thePlayer.hasPassedWaypointNumber == opponent8.hasPassedWaypointNumber && thePlayer.distanceToNextWaypoint > opponent8.distanceToNextWaypoint)
+            ) {
+                if (rubberband8) opponent8.pleaseSlowDown();
+                rank++;
+            } else {
+                if (rubberband8) opponent8.pleaseSpeedUp();
+            }
+        }
+
+
+        if (opponent9) {
+            bool rubberband9 = Vector3.Distance(opponent9.transform.position,thePlayer.transform.position) > tooFar;
+            if (thePlayer.hasPassedWaypointNumber < opponent9.hasPassedWaypointNumber
+            || (thePlayer.hasPassedWaypointNumber == opponent9.hasPassedWaypointNumber && thePlayer.distanceToNextWaypoint > opponent9.distanceToNextWaypoint)
+            ) {
+                if (rubberband9) opponent9.pleaseSlowDown();
+                rank++;
+            } else {
+                if (rubberband9) opponent9.pleaseSpeedUp();
+            }
+        }
+
+
+        if (opponent10) {
+            bool rubberband10 = Vector3.Distance(opponent10.transform.position,thePlayer.transform.position) > tooFar;
+            if (thePlayer.hasPassedWaypointNumber < opponent10.hasPassedWaypointNumber
+            || (thePlayer.hasPassedWaypointNumber == opponent10.hasPassedWaypointNumber && thePlayer.distanceToNextWaypoint > opponent10.distanceToNextWaypoint)
+            ) {
+                if (rubberband10) opponent10.pleaseSlowDown();
+                rank++;
+            } else {
+                if (rubberband10) opponent10.pleaseSpeedUp();
+            }
+        }
+
         return rank;        
     }
 
@@ -169,6 +249,12 @@ public class RacePositionDetector : MonoBehaviour
         if (opponent2) debugString += "\nOpponent 2 WP#"+opponent2.hasPassedWaypointNumber+" dist:"+opponent2.distanceToNextWaypoint.ToString("F1");
         if (opponent3) debugString += "\nOpponent 3 WP#"+opponent3.hasPassedWaypointNumber+" dist:"+opponent3.distanceToNextWaypoint.ToString("F1");
         if (opponent4) debugString += "\nOpponent 4 WP#"+opponent4.hasPassedWaypointNumber+" dist: "+opponent4.distanceToNextWaypoint.ToString("F1");
+        if (opponent5) debugString += "\nOpponent 5 WP#"+opponent5.hasPassedWaypointNumber+" dist: "+opponent4.distanceToNextWaypoint.ToString("F1");
+        if (opponent6) debugString += "\nOpponent 6 WP#"+opponent6.hasPassedWaypointNumber+" dist: "+opponent4.distanceToNextWaypoint.ToString("F1");
+        if (opponent7) debugString += "\nOpponent 7 WP#"+opponent7.hasPassedWaypointNumber+" dist: "+opponent4.distanceToNextWaypoint.ToString("F1");
+        if (opponent8) debugString += "\nOpponent 8 WP#"+opponent8.hasPassedWaypointNumber+" dist: "+opponent4.distanceToNextWaypoint.ToString("F1");
+        if (opponent9) debugString += "\nOpponent 9 WP#"+opponent9.hasPassedWaypointNumber+" dist: "+opponent4.distanceToNextWaypoint.ToString("F1");
+        if (opponent10) debugString += "\nOpponent 10 WP#"+opponent10.hasPassedWaypointNumber+" dist: "+opponent4.distanceToNextWaypoint.ToString("F1");
 
         if (updateThisText != null && newPosition != currentPosition) { // changed?
             switch (newPosition) {
@@ -182,6 +268,7 @@ public class RacePositionDetector : MonoBehaviour
                 case 8: updateThisText.text = "8th"; break;
                 case 9: updateThisText.text = "9th"; break;
                 case 10: updateThisText.text = "10th"; break;
+                case 11: updateThisText.text = "11th"; break;
                 default: updateThisText.text = "1st"; break;
             }
             currentPosition = newPosition;
