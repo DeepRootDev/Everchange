dc8480ebf048e594cf9cacdd7e9ef4a6dfc088d5
Christer Kaitila <breakdance@mcfunkypants.com>
2026-02-13 13:47:01 -0800

    if out of bounds, respawn with added height to avoid getting stuck

--- Assets/Scripts/OutOfBound.cs
+++ Assets/Scripts/OutOfBound.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 
 public class OutOfBound : MonoBehaviour
 {
+    public float respawnAddedHeight = 100f;
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
-        other.transform.position = PlayerCollisionManager.playerPositionOnExit + new Vector3(0,0,0);
+        if(other.CompareTag("Player")) 
+        {
+            Debug.Log("Player fell out of bounds! Respawning near last collision...");
+            other.transform.position = PlayerCollisionManager.playerPositionOnExit 
+                + new Vector3(0,respawnAddedHeight,0); // respawn a bit higher so you are less likely to get stuck
+        }
     }
 }
